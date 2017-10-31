41bb1088fa0551f0688c8c55af87bb478252b6de
Rodion Kryuchkov <rodion.kryuchkov@pipedrive.com>
2017-10-31 02:10:35 +0200

    improve all

--- src/app.js
+++ src/app.js
@@ -5,26 +5,61 @@ class App extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      score: 0
+      score: 0,
+      squares: null,
+      active: 0,
+      starts: 0
     }
     this.addScore = this.addScore.bind(this)
+    this.reportStop = this.reportStop.bind(this)
+    this.generateSquares = this.generateSquares.bind(this)
+    this.start = this.start.bind(this)
   }
+
+  componentDidMount () {
+    this.start()
+  }
+
   addScore (value) {
     this.setState({
-      score: this.state.score + (10 - value)
+      score: this.state.score + value
     })
   }
-  render () {
+
+  reportStop () {
+    this.setState({
+      active: this.state.active - 1
+    })
+  }
+
+  start () {
+    this.setState({
+      score: 0,
+      starts: this.state.starts + 1
+    })
+    this.generateSquares(36)
+  }
+
+  generateSquares (number) {
     let squares = []
-    for (let i = 0; i < 25; i++) {
-      squares.push(<Square key={i} addScore={this.addScore} />)
+    for (let i = 0; i < number; i++) {
+      squares.push(<Square key={this.state.starts * number + i} addScore={this.addScore} reportStop={this.reportStop} />)
     }
+    this.setState({
+      active: number,
+      squares: squares
+    })
+  }
+
+  render () {
     return (
       <div className='container'>
+        <div className='help'>Score points for squares before their value reaches 13</div>
         <div className='field'>
-          {squares}
+          {this.state.squares}
         </div>
         <div className='score'>Score: {this.state.score}</div>
+        <button onClick={this.start}>Start</button>
       </div>
     )
   }
--- src/square.js
+++ src/square.js
@@ -4,48 +4,69 @@ class Square extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      value: Math.floor(Math.random() * 10) + 3,
-      running: true,
+      value: Math.floor(Math.random() * 7),
+      running: false,
       won: false
     }
     this.handleClick = this.handleClick.bind(this)
   }
+
   componentDidMount () {
+    this.setState({
+      running: true
+    })
     this.timer = setInterval(
       () => this.tick(),
       1000
     )
   }
+
+  componentWillUnmount () {
+    clearInterval(this.timer)
+  }
+
+  stop () {
+    clearInterval(this.timer)
+    this.setState({
+      running: false
+    })
+    this.props.reportStop()
+  }
+
   tick () {
-    if (this.state.running) {
-      this.setState({ value: this.state.value - 1 })
-    }
-    if (this.state.value === 0) {
-      this.setState({ running: false })
-      clearInterval(this.time)
+    if (this.state.value === 13) {
+      this.stop()
+    } else {
+      this.setState({
+        value: this.state.value + 1
+      })
     }
   }
+
   getBackground () {
     if (this.state.won) {
       return 'won'
     } else if (!this.state.running) {
       return 'stopped'
-    } else if (this.state.value > 5) {
+    } else if (this.state.value < 7) {
       return 'green'
-    } else if (this.state.value > 3) {
+    } else if (this.state.value < 10) {
       return 'yellow'
     } else {
       return 'red'
     }
   }
+
   handleClick () {
     if (this.state.running) {
+      this.stop()
+      this.setState({
+        won: true
+      })
       this.props.addScore(this.state.value)
-      this.setState({ won: true })
     }
-    this.setState({ running: false })
-    clearInterval(this.time)
   }
+
   render () {
     return (
       <div className={'square ' + this.getBackground()} onClick={this.handleClick}>
